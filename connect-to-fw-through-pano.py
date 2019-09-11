e476380374825f9106cc7fbf2594d1aeb0b8c01d
brtrost <trostb@gmail.com>
2019-09-11 12:33:59 -0500

    Updating to include logging facility as part of example.

--- connect-to-fw-through-pano.py
+++ connect-to-fw-through-pano.py
@@ -10,6 +10,7 @@ import ssl
 import os
 import sys
 import requests
+import logging
 import xml.etree.ElementTree as ET
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -22,7 +23,16 @@ parser.add_argument("-f", "--querytarget", help="IP address of the panorama")
 parser.add_argument("-k", "--apikey", help="API Key")
 parser.add_argument("-u", "--username", help="User login")
 parser.add_argument("-p", "--password", help="Login password")
+parser.add_argument("-l", "--log", default="INFO")
+
 args = parser.parse_args()
+
+try:
+    lnum = getattr(logging, args.log.upper())
+except:
+    lnum = 20
+
+logging.basicConfig(format='%(message)s', level=lnum)
 
 if args.querytarget:
     querytarget = args.querytarget
@@ -32,8 +42,7 @@ else:
 
 def printdes(s, o=''):
     print ("\n  " + sys.argv[0] + " - " + s)
-    olist = o.split(",")
-    if 'printul' in olist:
+    if 'printul' in o.split(","):
         tl = len(sys.argv[0]) + len(s) + 7
         print("-"*tl)
     else:
@@ -42,8 +51,7 @@ def printdes(s, o=''):
 
 def send_api_request(url, values):
     try:
-        response = requests.get(url, params=values, verify=False)
-        return response.text
+        return requests.get(url, params=values, verify=False).text
     except:
         raise ValueError("API call not successful.")
 
@@ -80,7 +88,8 @@ def fetch_api_key():
 def main():
 
     apikey = fetch_api_key()
-    printdes("show devices and check zone settings", o='printul')
+
+    printdes("Example script", o='printul')
 
     cmd = '<show><system><info></info></system></show>'
     showcmd = {'type': 'op', 'cmd': cmd, 'key': apikey}
@@ -89,44 +98,43 @@ def main():
     print ("Target device " + querytarget + "\n")
 
     showsysteminfoxml = ET.fromstring(send_api_request(url, showcmd))
-    model = showsysteminfoxml.find('result').find('system').find('model').text
+    family = showsysteminfoxml.find('result/system/family').text
 
-    panoramamodel = ['M-100', 'M-500', 'M-200', 'M-600']
+    panoramafamily = ['m']
 
-    if model in panoramamodel:
+    if family in panoramafamily:
         cmd = '<show><devices><connected></connected></devices></show>'
-        renamedict = {'type': 'op', 'cmd': cmd, 'key': apikey}
+        connectiondict = {'type': 'op', 'cmd': cmd, 'key': apikey}
 
         url = "https://%s/api" % querytarget
-        print ("Querying...." + url + "\n")
+        logging.info("Querying...." + url + "\n")
 
-        showconn = ET.fromstring(send_api_request(url, renamedict))
+        showconn = ET.fromstring(send_api_request(url, connectiondict))
 
-        for child in showconn.findall('.//devices/entry'):
-            serial = child.find('serial').text
-            hostname = child.find('hostname').text
-            hip = child.find('ip-address').text
-            model = child.find('model').text
+        for fw in showconn.findall('.//devices/entry'):
+            serial = fw.find('serial').text
+            hostname = fw.find('hostname').text
+            hip = fw.find('ip-address').text
+            model = fw.find('model').text
 
-            state = child.find('ha').find('state').text
+            state = fw.find('ha/state').text
 
             if state == 'active':
                 xpath = '/config/devices/entry[@name=\'localhost.localdomain\']/vsys'
-                lookupdict = {'type': 'config', 'action': 'get',
-                              'key': apikey, 'xpath': xpath, 'target': serial}
-                zpp = ''
+                vsysfinddict = {'type': 'config', 'action': 'get',
+                                'key': apikey, 'xpath': xpath, 'target': serial}
 
                 url = "https://%s/api" % querytarget
-                print("%s - %s - %s - %s\n" % (hostname, serial, hip, model))
-                y = ET.fromstring(send_api_request(url, lookupdict).encode('utf-8'))
-                print(" Finding VSYS")
+                logging.warning(" %s - %s - %s - %s\n" % (hostname, serial, hip, model))
+                showvsys = ET.fromstring(send_api_request(url, vsysfinddict))
+                logging.info(" Finding VSYS")
 
-                for vsysentry in y.findall('.//vsys/entry'):
-                    print(" - Found VSYS entry %s - %s" %
-                          (vsysentry.get('name'), vsysentry.find('display-name').text))
+                for vsysentry in showvsys.findall('.//vsys/entry'):
+                    logging.info(" - Found VSYS entry %s - %s" %
+                                 (vsysentry.get('name'), vsysentry.find('display-name').text))
                 print("")
     else:
-        print("Not a panorama device, exiting.\n")
+        logging.info("Not a panorama device, exiting.\n")
 
 
 if __name__ == '__main__':
